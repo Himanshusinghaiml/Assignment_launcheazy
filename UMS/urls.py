6415df733e34042930cec28259e22ce617d74569
Himanshusinghaiml <himanshusingh945443@gmail.com>
2024-02-24 14:05:09 +0530

    registration and login page completed and also model relationship

--- UMS/urls.py
+++ UMS/urls.py
@@ -3,7 +3,12 @@ from .views import UserSignupView
 # from .views import *
 from . import views
 urlpatterns = [
-    path('signup/', UserSignupView.as_view(), name='user-signup'),
-    path('',views.signupuser,name='sign')
+    path('signup/', UserSignupView.as_view(), name='user-signup'), 
+    path('',views.org_name,name='orgname'),
+    path('signup-page/',views.signupuser,name='signupuser'),
+    path('login-page/',views.loginpage,name='loginpage'),
+    path('user-login/', views.login_view, name='userlogin'),
+    path('admin-dashboard/',views.admin_dash,name='admin-dash'),
+    path('member-requests/',views.member,name='member'),
     
 ]
--- UMS/views.py
+++ UMS/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 # views.py
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import UserSignupSerializer
-
-from rest_framework import status
-from rest_framework.response import Response
+from django.contrib.auth import authenticate, login
+ 
 from rest_framework.views import APIView
 from .serializers import UserSignupSerializer
 
@@ -21,12 +20,44 @@ def user_signup(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def signupuser(req):
-    return render(req,'singup.html')
- 
+    return render(req,'signup.html')
+
+def org_name(req):
+    return render(req,'org_name.html')
+
 class UserSignupView(APIView):
     def post(self, request):
         serializer = UserSignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return  HttpResponse("successfully created user")
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return  redirect('login-page')
+        return  redirect('signupuser')
+
+
+def loginpage(req):
+    return render(req,'login.html')
+
+ 
+def admin_dash(req):
+    return render(req,'admin_dash.html')
+
+from django.contrib.auth import authenticate, login
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('admin-dash')  # Redirect to the dashboard page after successful login
+        else:
+            # Authentication failed
+            return render(request, 'login.html', {'error_message': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
+def member(req):
+    return render(req,'member-req.html')
